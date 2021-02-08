4b2ae9c4bd293175694546e0c86e272af9088d73
Devon Poon Woo <devon.poonwoo@gmail.com>
2021-02-07 20:22:23 -0500

    Added Player movement and jumping.

--- ILP1-Unity/Assets/Scripts/PlayerController.cs
+++ ILP1-Unity/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField]
+    Rigidbody2D rBody;
+
+    [SerializeField]
+    float moveSpeed = 1;
+
+    [SerializeField]
+    float jumpForce = 1;
+
+    Vector3 moveVec = Vector3.zero;
+
     void Start()
     {
         
@@ -13,6 +24,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //move player left and right
+        moveVec.x = Input.GetAxis("Horizontal");
+        transform.Translate(moveVec * moveSpeed);
+        moveVec.Set(0, 0, 0);
+
+        //jump
+        if (canJump == true && Input.GetAxis("Jump") == 1)
+        {
+            rBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            canJump = false;
+        }
     }
+
+    public bool canJump = true;
 }
---
+++ ILP1-Unity/Assets/Triggger2DCallback.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triggger2DCallback : MonoBehaviour
+{
+    [SerializeField]
+    public PlayerController playerController;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        playerController.canJump = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        playerController.canJump = false;
+    }
+}
